311315cff40f12d618334baaaa725fcd8dd21b2e
JoeyG <erjo.guce214@gmail.com>
2023-01-22 23:58:41 -0500

    Styled component

--- src/components/UserInputComponents/PurchaseLoanComponents/PurchasePrice.jsx
+++ src/components/UserInputComponents/PurchaseLoanComponents/PurchasePrice.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BsPencil } from 'react-icons/bs'
 
 const PurchasePrice = () => {
   return (
-    <div className=''>
-      
+    <div className='flex flex-col justify-between w-80 h-44 p-4 bg-zinc-700 shadow-slate-600 shadow-2xl rounded-xl'>
+      <BsPencil className=' relative w-14 h-14 p-2 left-56 bottom-2  fill-blue-600 cursor-pointer'/>
+      <div className='flex flex-row justify-between items-center'>
+        <p className='text-white font-bold text-3xl'>$0</p>
+        <p className='text-white font-semibold text-md'>Purchase Price</p>
+      </div>
+      <div>
+        <input className='w-72' type='range' name='purchase-price' id='purchase-price' />
+      </div>
     </div>
   )
 }
